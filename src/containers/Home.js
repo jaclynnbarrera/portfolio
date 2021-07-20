dd89e9ddece92d217b352fee471ed9f0b8ec49f0
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-07-19 23:29:31 -0400

    create Home container

---
+++ src/containers/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class HomeContainer extends React.Component {
+  render() {
+    console.log("home container");
+    return <div>JACLYN BARRERA</div>;
+  }
+}
+
+export default HomeContainer;

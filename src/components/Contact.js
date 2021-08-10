515eedff10976a6ed3d2e2722a815c99ac947d94
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-08-10 14:42:09 -0400

    add href mailto in contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,19 @@ const Contact = () => {
     <div className="contact">
       <h2>WHERE TO FIND ME</h2>
 
-      <a href="https://github.com/jaclynnbarrera">
+      <a href="mailto: jaclynnbarrera@gmail.com" target="_blank">
+        <p>Email ↗️</p>
+      </a>
+
+      <a href="https://github.com/jaclynnbarrera" target="_blank">
         <p>GitHub ↗️</p>
       </a>
 
-      <a href="https://github.com/jaclynnbarrera">
+      <a href="https://github.com/jaclynnbarrera" target="_blank">
         <p>Twitter ↗️</p>
       </a>
 
-      <a href="https://github.com/jaclynnbarrera">
+      <a href="https://github.com/jaclynnbarrera" target="_blank">
         <p>Medium ↗️</p>
       </a>
     </div>

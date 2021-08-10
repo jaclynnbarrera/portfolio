78504766d889d4a10c906958ed59b81ccc237eae
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-08-10 14:22:54 -0400

    set up HashRouter and NavLink for Single Page routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
 import Home from "./containers/Home";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import { Route, NavLink, HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <HashRouter>
+      <div className="nav">
+        <p>
+          <NavLink to="/">Jaclyn Barrera</NavLink>
+        </p>
+        <p>
+          <NavLink to="/work">Work</NavLink>
+        </p>
+        <p>
+          <NavLink to="/about">About</NavLink>
+        </p>
+      </div>
+
+      <div className="content">
+        <Route exact path="/" component={Home} />
+        <Route path="/work" component={Projects} />
+        <Route path="/about" component={About} />
+      </div>
+    </HashRouter>
   );
 }
 
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,28 +4,17 @@ import Projects from "../components/Projects";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-import { Route, NavLink, HashRouter } from "react-router-dom";
 
 class Home extends React.Component {
   render() {
-    console.log("home container");
     return (
-      <HashRouter>
-        {/* <Nav /> */}
-        <div className="nav">
-          <p>Jaclyn Barrera</p>
-          <p>Work</p>
-          <p>About</p>
-        </div>
-
-        <div className="content">
-          <Intro />
-          <Projects />
-          <About />
-          <Contact />
-          <Footer />
-        </div>
-      </HashRouter>
+      <div className="content">
+        <Intro />
+        <Projects />
+        <About />
+        <Contact />
+        <Footer />
+      </div>
     );
   }
 }
